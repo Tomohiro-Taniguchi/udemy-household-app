b6d1cab2f03a0fd556d87d4eafec5bda9649eff4
Tomohiro-Taniguchi <m241731@hiroshima-u.ac.jp>
2025-05-16 09:53:07 +0900

    firestoreからのデータ取得を実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
@@ -9,8 +10,45 @@ import AppLayout from "./components/layout/AppLayout";
 import { theme } from "./theme/theme";
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
+import { Transaction } from "./types/index";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./firebase";
 
 function App() {
+  // Firestoreエラーかどうかを判定する型ガード
+  function isFireStoreError(
+    err: unknown
+  ): err is { code: string; message: string } {
+    return typeof err === "object" && err != null && "code" in err;
+  }
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    const fetcheTransactions = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "Transactions"));
+        console.log(querySnapshot);
+        const transactionsData = querySnapshot.docs.map((doc) => {
+          // doc.data() is never undefined for query doc snapshots
+          // console.log(doc.id, " => ", doc.data());
+          return {
+            ...doc.data(),
+            id: doc.id,
+          } as Transaction;
+        });
+        console.log(transactionsData);
+        setTransactions(transactionsData);
+      } catch (err) {
+        // error
+        if (isFireStoreError(err)) {
+          console.error("firestoreのエラーは：", err);
+        } else {
+          console.error("一般的なエラーは：", err);
+        }
+      }
+    };
+    fetcheTransactions();
+  }, []);
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
